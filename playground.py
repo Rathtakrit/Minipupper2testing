594108c3a8bb7be8a307c159040294875c1afcbd
Rathtakrit <kiwimama@hotmail.co.th>
2025-06-19 07:20:00 +0000

    added the balance code(cannot be execute yet since did not import the modify movement script)

--- playground.py
+++ playground.py
@@ -2,8 +2,5 @@ from src.MovementGroup import MovementGroups
 import time
 
 Move = MovementGroups()
-
-Move.look_right()
-Move.look_upperright()
-Move.move_forward()
-Move.stop()
+while True:
+	Move.gait_uni(0.1)
